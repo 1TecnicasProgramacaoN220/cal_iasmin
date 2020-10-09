19dd86bc69992c8aeabf779b8974e0a6c8f9d0b3
gentil <gentil@gentil-PC>
2020-10-08 21:10:03 -0300

    commit do dia 08

--- src/pct/Calculadora.java
+++ src/pct/Calculadora.java
@@ -17,6 +17,8 @@ public class Calculadora {
         
         int n1, n2,total;
         
+        int op;
+        
         // criaçao e instancia de um projeto
         
         Scanner entrada = new Scanner(System.in); 
@@ -25,6 +27,18 @@ public class Calculadora {
         
         System.out.println("\n\t\t\t-- calculadora--\n");
         
+        //menu
+        System.out.println("[1]. soma");
+        System.out.println("[2]. subtração");
+        System.out.println("[3]. divisão");
+        System.out.println("[4]. multiplicação");
+        System.out.println("[5]. saida");
+        
+        //obter a opção escolhida pelo usuario
+        System.out.println("\n\t opção: ");
+        op = entrada.nextByte();
+        
+        if (op == 1){
         System.out.println("soma simples");
 
         //Entrada
@@ -41,7 +55,8 @@ public class Calculadora {
         //Saída
         
         System.out.println("\n\t\t" + n1 + " + " + n2+"=" + total);
-        
+       
+            
          //Entrada de subtraçao
         System.out.print("\tinforme N1: ");
         n1 = entrada.nextInt ();
@@ -84,8 +99,21 @@ public class Calculadora {
         //Saída
         
         System.out.println("\n\t\t" + n1 + " * " + n2+"=" + total);
+         } else if(op == 2){
+            System.out.println("subtração");
         
-         
+         } else if (op == 3){
+             System.out.println("divisão");
+             
+         } else if (op == 4){
+             System.out.println("multiplicação");
+        
+         } else if (op == 5){
+             System.out.println("opção" + op + " Incorreta");
+         }
+        
+        
+        
     }
     
 }
